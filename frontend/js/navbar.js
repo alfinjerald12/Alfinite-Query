602242c337c6d9204039169d15257131015d30d4
Alfi Alfi <alfinjerald12.com>
2026-06-27 16:33:37 +0530

    Make website fully responsive and fix mobile navigation

---
+++ frontend/js/navbar.js
@@ -0,0 +1,10 @@
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+if (menuToggle && navLinks) {
+
+    menuToggle.addEventListener("click", () => {
+        navLinks.classList.toggle("show");
+    });
+
+}
